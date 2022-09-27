25714923c31d051d39b81b0709226a6e7ff2470d
karl wernsman <wernsmank@gmail.com>
2022-09-26 17:02:35 -0700

    Step C - redirect

--- auth/user.js
+++ auth/user.js
@@ -5,8 +5,24 @@ const signOutLink = document.getElementById('sign-out-link');
 // make sure we have a user!
 // > Part C:
 //     - get the user (if there is one)
+const user = getUser();
 //     - check if there is not a user and if so do the redirect
-//       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+if (!user) {
+    // const base =
+    //     location.pathname === '/'
+    //         ? './'
+    //         : '../';
+
+    let base = null;
+    if (location.pathname === '/') {
+        base = './';
+    } else {
+        base = '../';
+    }
+
+    //       redirect code: location.replace(`/auth/?redirectUrl=${encodeURIComponent(location)}`);
+    location.replace(`${base}/auth/?redirectUrl=${encodeURIComponent(location)}`);
+}
 
 // > Part B: attach event listener to signOutLink that calls signOutUser
 signOutLink.addEventListener('click', signOutUser);
